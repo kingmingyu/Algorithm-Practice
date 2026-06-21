4243dc7968527477257168b89c74eb8ff9e5e612
kingmingyu <90828383+kingmingyu@users.noreply.github.com>
2026-06-21 15:49:13 +0900

    [level 2] Title: 주식가격, Time: 8.87 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length - 1; i++) {
+            int cnt = 0;
+            for(int j = i + 1; j < prices.length; j++) {
+                if(prices[i] <= prices[j]) {
+                    cnt++;
+                }
+                else {
+                    cnt++;
+                    break;
+                }
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+}
