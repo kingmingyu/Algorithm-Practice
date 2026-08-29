be0f0831d6e05ce884f5f9e755d2919982d6f33b
kingmingyu <90828383+kingmingyu@users.noreply.github.com>
2026-08-29 16:17:40 +0900

    [level 2] Title: H-Index, Time: 1.28 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,28 +3,19 @@ import java.io.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        
-        Integer[] citationsInte = 
-            Arrays.stream(citations).boxed().toArray(Integer[]::new);
-        
-        // 인용된 횟수 내림차순 정렬
-        Arrays.sort(citationsInte, Collections.reverseOrder());
-        
-        // 앞에서부터 순차적으로 돌면서 i번째 해당하는 값이 i개 이상 있는지 체크
-        int h = citationsInte.length;
-        while(true) {
-            int cnt = 0;
-            for(int i = 0; i < citationsInte.length; i ++) {
-                if(citationsInte[i] >= h) {
-                    // System.out.println("h: " + h + " / citationsInte[i]: " + citationsInte[i]);
-                    cnt++;
-                    if(cnt >= h) {
-                        return h;
-                    }
-                }
-            }
-            h--;
+        // Integer 배열로 변경
+        Integer[] citationsSort = new Integer[citations.length];
+        for(int i = 0; i < citations.length; i++) {
+            citationsSort[i] = citations[i];
         }
+        
+        Arrays.sort(citationsSort, Comparator.reverseOrder());
+        for(int i = 0; i < citationsSort.length; i++) {
+            // h번 이상 인용된 논문
+            if(i + 1 > citationsSort[i]) {
+                return i;
+            }
+        }
+        return citationsSort.length;
     }
 }
