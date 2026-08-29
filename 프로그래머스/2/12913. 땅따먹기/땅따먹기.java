b996878ed70b691bcea5c6cbb01baa0a3132e011
kingmingyu <90828383+kingmingyu@users.noreply.github.com>
2026-08-29 19:56:12 +0900

    [level 2] Title: 땅따먹기, Time: 18.20 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    int solution(int[][] land) {
+        // dp를 이용
+        int[][] dp = new int[land.length][4];
+        
+        // dp 초기값 설정
+        for(int i = 0; i < 4; i++) {
+            dp[0][i] = land[0][i];
+        }
+        
+        for(int i = 1; i < land.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][1], Math.max(dp[i-1][2], dp[i-1][3])) + land[i][0];
+            dp[i][1] = Math.max(dp[i-1][0], Math.max(dp[i-1][2], dp[i-1][3])) + land[i][1];
+            dp[i][2] = Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][3])) + land[i][2];
+            dp[i][3] = Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][2])) + land[i][3];
+        }
+        
+        int answer = 0;
+        for(int i = 0; i < 4; i++) {
+            if(answer < dp[land.length-1][i]) answer = dp[land.length-1][i];
+        }
+        return answer;
+    }
+}
