7e1076246b9a548480de7539d938d2dc25a2d458
kingmingyu <90828383+kingmingyu@users.noreply.github.com>
2026-09-02 14:59:12 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> clo = new HashMap<>();
+        HashMap<String, Integer> cMap = new HashMap<>();
         
         for(String[] c : clothes) {
-            clo.put(c[1], clo.getOrDefault(c[1], 0) + 1);
+            cMap.put(c[1], cMap.getOrDefault(c[1], 0) + 1);
         }
         
         int answer = 1;
-        for(String k : clo.keySet()) {
-            answer *= clo.get(k) + 1;
+        for(String k : cMap.keySet()) {
+            answer *= cMap.get(k) + 1; // 선택하지 않는 경우 +1
         }
         
         return answer - 1;
