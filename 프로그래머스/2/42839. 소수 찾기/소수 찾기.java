aa5c87219654a940c682daeb35aface6aa14996e
kingmingyu <90828383+kingmingyu@users.noreply.github.com>
2026-09-02 15:20:34 +0900

    [level 2] Title: 소수 찾기, Time: 77.44 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,35 +2,38 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public static HashSet<Integer> primary = new HashSet<>();
-    
+    public static HashSet<Integer> pSet;
     public int solution(String numbers) {
-        dfs("", numbers);
+        pSet = new HashSet<>();
+        boolean[] visited = new boolean[numbers.length()];
         
-        int answer = 0;
-        for(Integer i : primary) {
-            if(isPrimary(i)) {
-                answer++;
-                // System.out.println(i);
+        for(int i = 0; i < numbers.length(); i++) {
+            String cur = String.valueOf(numbers.charAt(i));
+            visited[i] = true;
+            dfs(cur, visited, numbers);
+            visited[i] = false;
+        }
+        return pSet.size();
+    }
+    
+    public static void dfs(String cur, boolean[] visited, String numbers) {
+        if(isPrimary(cur)) {
+            pSet.add(Integer.valueOf(cur));
+        }
+        
+        for(int i = 0; i < numbers.length(); i++) {
+            if(visited[i] == false) {
+                visited[i] = true;
+                dfs(cur + numbers.charAt(i), visited, numbers);
+                visited[i] = false;
             }
         }
-        return answer;
     }
     
-    public static void dfs(String comb, String others) {
-        if(!comb.equals("")) {
-            primary.add(Integer.parseInt(comb));
-        }
-        
-        for(int i = 0; i < others.length(); i++) {
-            dfs(comb + others.charAt(i),
-               others.substring(0, i) + others.substring(i+1));
-        }
-    }
-    
-    public static boolean isPrimary(int n) {
+    public static boolean isPrimary(String cur) {
+        int n = Integer.valueOf(cur);
         if(n == 0 || n == 1) return false;
-        for(int i = 2; i <= Math.sqrt(n); i++) {
+        for(int i = 2; i < n; i++) {
             if(n % i == 0) return false;
         }
         return true;
